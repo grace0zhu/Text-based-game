f308ed98a2ed85a2fcfeb673cb4327b12b91cf32
grace0zhu <67290711+grace0zhu@users.noreply.github.com>
2024-07-30 16:25:22 -0500

    Create main.cpp
    intro to game, user sees texts and inputs their player name

---
+++ main.cpp
@@ -0,0 +1,22 @@
+// Grayson Zhu textbased game
+// main.cpp
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+//variables
+    string name;
+  
+//intro text
+    cout << "Hello, Battlers and Broads!"; endl;
+    cout << "You all are now purveyors of the new text-based game which will determine; endl;
+    cout << "the fate of the entire world! Bahahahahaha!!!!!"; endl;
+    cout << "Choose your player name now: "; endl;
+    getline (cin, name);
+
+  
+  
+    return 0;
+}
