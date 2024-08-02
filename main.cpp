38cc30168a11de69383c70c998b26cb366df8fbf
grace0zhu <67290711+grace0zhu@users.noreply.github.com>
2024-08-02 13:45:07 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -17,7 +17,7 @@ int main() {
     
 //intro text
     cout << "Hello, Battlers and Broads!"; endl;
-    cout << "You all are now purveyors of the new text-based game which will determine; endl;
+    cout << "You all are now purveyors of the new text-based game which will determine"; endl;
     cout << "the fate of the entire world! Bahahahahaha!!!!!"; endl;
     cout << "Choose your player name now: "; endl;
     getline (cin, name);
